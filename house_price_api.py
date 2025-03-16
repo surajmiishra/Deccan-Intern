44657159be9ee0647264efb98fac8a886f432af0
Suraj Mishra <143419926+surajmiishra@users.noreply.github.com>
2025-03-16 13:25:02 +0530

    Update house_price_api.py

--- house_price_api.py
+++ house_price_api.py
@@ -31,4 +31,5 @@ def predict():
 
 # Run the API
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+
